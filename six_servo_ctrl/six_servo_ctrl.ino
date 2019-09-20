659653885c2e0e3a430f1937ea14c27231a96725
Arjun Narayan <arjunn@rogers.com>
2019-09-20 11:28:00 -0400

    cleaned up indexing, added write function

--- six_servo_ctrl/six_servo_ctrl.ino
+++ six_servo_ctrl/six_servo_ctrl.ino
@@ -1,30 +1,57 @@
 #include <Servo.h>
 
-Servo servos[6];  // create servo object to control a servo
- 
-// twelve servo objects can be created on most boards
+// Servo movement limits in degrees
+#define mechanical_angle_limit_max    175
+#define mechanical_angle_limit_min    5
 
-int pos = 0;    // variable to store the servo position
+#define functional_angle_limit_max    175
+#define functional_angle_limit_min    5
+
+// Pins are offset from their array mapping to their PWM pin mapping
+#define servo_pin_offset  2
+typedef enum {
+  servo_1 = 0,
+  servo_2,
+  servo_3,
+  servo_4,
+  servo_5,
+  servo_6,
+  num_servos
+} servos_idx_e;
+
+
+Servo servos[num_servos];
+int pos = 0;
 
 void setup() {
-  for (int i = 0; i<6;i++){
-    servos[i].attach(i+2);
+    servos[servo_1].attach(servo_1 + servo_pin_offset);
+    servos[servo_2].attach(servo_2 + servo_pin_offset);
+    servos[servo_3].attach(servo_3 + servo_pin_offset);
+    servos[servo_4].attach(servo_4 + servo_pin_offset);
+    servos[servo_5].attach(servo_5 + servo_pin_offset);
+    servos[servo_6].attach(servo_6 + servo_pin_offset);    
+}
+
+void write_servo(int target_angle, servos_idx_e target_index) {
+  if (target_index != num_servos) {
+    // Ensure target angle within mechanical & functional liimits
+    if (target_angle < mechanical_angle_limit_max && target_angle > mechanical_angle_limit_min &&
+        target_angle < functional_angle_limit_max && target_angle > functional_angle_limit_min) {
+          servos[target_index].write(target_angle);
+        }
   }
-//  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      for (int serv=0; serv<6;serv+=1){
-        servos[serv].write(pos);              // tell servo to go to position in variable 'pos'
-        delay(1);                       // waits 15ms for the servo to reach the position
+      for (int servo = 0; servo < 6;servo += 1){
+        write_servo(pos, (servos_idx_e) servo);
+        delay(1);
       }
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    for (int serv=0; serv<6;serv+=1){
-        servos[serv].write(pos);              // tell servo to go to position in variable 'pos'
-        delay(1);                       // waits 15ms for the servo to reach the position
-    }                  // waits 15ms for the servo to reach the position
+    for (int servo = 0; servo < 6; servo += 1){
+        write_servo(pos, (servos_idx_e) servo);
+        delay(1);
   }
 }
