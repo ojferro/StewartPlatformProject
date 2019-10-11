182f454f6ff2ea4f36a47858a17ed81e44e038ca
saeejithnair <saeejithn@gmail.com>
2019-10-10 21:46:16 -0400

    Removed more dead code

--- six_servo_ctrl/inv_kin.hpp
+++ six_servo_ctrl/inv_kin.hpp
@@ -73,22 +73,12 @@ quat bp_translations[num_servos] = {
 
 // Defines rotation from base to platform. Only one quaternion is needed because the rotation matrix will be same for all servos
 quat bp_rot = {R_NEUTRAL_X, R_NEUTRAL_Y, R_NEUTRAL_Z, R_NEUTRAL_W};
-// quat bp_rots[num_servos] = {
-//     {R_NEUTRAL_X, R_NEUTRAL_Y, R_NEUTRAL_Z, R_NEUTRAL_W},
-//     {R_NEUTRAL_X, R_NEUTRAL_Y, R_NEUTRAL_Z, R_NEUTRAL_W},
-//     {R_NEUTRAL_X, R_NEUTRAL_Y, R_NEUTRAL_Z, R_NEUTRAL_W},
-//     {R_NEUTRAL_X, R_NEUTRAL_Y, R_NEUTRAL_Z, R_NEUTRAL_W},
-//     {R_NEUTRAL_X, R_NEUTRAL_Y, R_NEUTRAL_Z, R_NEUTRAL_W},
-//     {R_NEUTRAL_X, R_NEUTRAL_Y, R_NEUTRAL_Z, R_NEUTRAL_W}
-// };
 
 void QuatMult(quat r, quat A, quat B){
     r[0] = B[0] * A[3] - B[1] * A[2] + B[2] * A[1] + B[3] * A[0];
     r[1] = B[0] * A[2] + B[1] * A[3] - B[2] * A[0] + B[3] * A[1];
     r[2] = -B[0] * A[1] + B[1] * A[0] + B[2] * A[3] + B[3] * A[2];
     r[3] = -B[0] * A[0] - B[1] * A[1] - B[2] * A[2] + B[3] * A[3];
-//    r3 = -x2*y1 + y2*x1 + z2*w1 + w2*z1
-//    z = w1z2 + x1y2 - y1x2 + z1w2
 }
 
 float DotProd(float v1[], float v2[]){
