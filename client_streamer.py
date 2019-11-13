9b3374575f77827ee70aeb72aa042011d8c84d63
Saeejith Nair <saeejithn@gmail.com>
2019-11-13 16:22:26 -0500

    Tested local client/server video streaming successfully

--- client_streamer.py
+++ client_streamer.py
@@ -1,9 +1,11 @@
 # import the necessary packages
 from imutils.video import VideoStream
-import imagezmq
+import imutils.video
+from imagezmq import imagezmq
 import argparse
 import socket
 import time
+import cv2
  
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -19,11 +21,17 @@ sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
 rpiName = socket.gethostname()
-vs = VideoStream(usePiCamera=True).start()
-#vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+vs = VideoStream(src=0).start()
 time.sleep(2.0)
+
+num_processed_frames = 0
  
 while True:
 	# read the frame from the camera and send it to the server
+	print("reading frame {}".format(num_processed_frames))
 	frame = vs.read()
+	num_processed_frames+=1
+	# cv2.imshow("Video Stream", frame)
+	# cv2.waitKey(10)
 	sender.send_image(rpiName, frame)
