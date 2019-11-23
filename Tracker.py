b12e46e52507e26d282209c054c1cfcd86f88243
ojferro <ojferro@gmail.com>
2019-11-22 21:08:46 -0500

    Made the tracker callable from main

--- Tracker.py
+++ Tracker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 #Works with python 3.7.2
 #Dependencies:
 #   opencv-python
@@ -14,42 +16,49 @@ def get_bbox_centre(bbox):
   return (int(bbox[0] + bbox[2]/2), int(bbox[1] + bbox[3]/2))
 
 def get_centres_avg(bbox_buffer):
+  print(bbox_buffer)
   return (int(np.average(np.array(bbox_buffer)[:,0])), int(np.average(np.array(bbox_buffer)[:,1])))
 
 # tracker = cv2.TrackerCSRT_create()
 tracker = cv2.TrackerKCF_create()
 tracker_enabled = False
 bbox = ()
-bbox_buffer_ln = 5 # num bboxes to be 
+bbox_buffer_ln = 5 # num bboxes to be averaged
 bbox_buffer=[]
+cnt=0
 
 # Read video
-video = cv2.VideoCapture("dashcam_video.mp4")
+video = cv2.VideoCapture(0)#("dashcam_video.mp4")
 
-# Exit if video not opened.
-if not video.isOpened() or not video.read()[0]:
-  print ("Could not open video")
+def init_cv():
+  # Exit if video not opened.
+  if not video.isOpened() or not video.read()[0]:
+    print ("Could not open video")
 
-ok, frame = video.read()
-# print("!!!!!!!!!!!!!! frame size {}x{}".format(frame.shape[0], frame.shape[1]))
-# cv2.waitKey(500)
+  ok, frame = video.read()
 
-video.set(0, min_to_ms(5.0))
-video.set(3, video.read()[1].shape[0]*1)
-video.set(4, video.read()[1].shape[1]*1)
+  video.set(0, min_to_ms(5.0))
+  video.set(3, video.read()[1].shape[0]*1)
+  video.set(4, video.read()[1].shape[1]*1)
 
-# Read first frame.
-ok, frame = video.read()
-if not ok:
-  print ('Cannot read video file')
+  # Read first frame.
+  ok, frame = video.read()
+  if not ok:
+    print ('Cannot read video file')
 
-cnt=0
-while True:
-  cnt+=1
+def get_cv_error():
+  global cnt
+  global tracker_enabled
+  global bbox_buffer
+  global fps
+
+
+  cnt +=1
+
   # Read a new frame
   ok, frame = video.read()
   if not ok:
-      break
+      return False
   
   # Start timer
   timer = cv2.getTickCount()
@@ -69,7 +78,7 @@ while True:
         p1 = (int(bbox[0]), int(bbox[1]))
         p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
         cv2.rectangle(frame, p1, p2, (255,0,0), 3, 1)
-        cv2.circle(frame,centre,3,(0,0,255),1) #Drawing circle is apparently super expensive, USE FOR DEBUGGING ONLY!
+        cv2.circle(frame,centre,3,(0,0,255),1)
     else:
         # Tracking failure
         cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
@@ -96,6 +105,11 @@ while True:
   # Display result
   cv2.imshow("frame", frame)
   cv2.waitKey(10)
+  
+  return get_centres_avg(bbox_buffer) if tracker_enabled else -1
 
   # Exit if ESC pressed
-  if key == 27 : break
+  # if key == 27 : break
+
+def error_to_angles(err_x, err_y):
+  return [0,0,0]
